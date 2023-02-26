e57c7f7c99f17ff6c9062885cf8e13f3ffc2429b
椰香猪 <coconut.piglet@outlook.com>
2023-02-26 09:44:11 +0800

    Create userscript.js for Instagram

---
+++ Instagram/userscript.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @name         Coconut Instagram
+// @author       Coconut Piglet
+// @version      0.1
+// @description  Make Instagram great again.
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=instagram.com
+// @namespace    https://instagram.com/
+// @match        https://instagram.com/*
+// @match        https://www.instagram.com/*
+// @run-at       document-start
+// @grant        GM_download
+// ==/UserScript==
+
+(function () {
+  "use strict";
+})();
